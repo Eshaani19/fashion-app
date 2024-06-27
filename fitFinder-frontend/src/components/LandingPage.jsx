6eb524fb7ba9fb5c510cba712a79e0bc527297ee
Eshaani Arvind <eshaaniarvind@gmail.com>
2024-06-27 23:59:26 +0530

    changing landing page design

--- fitFinder-frontend/src/components/LandingPage.jsx
+++ fitFinder-frontend/src/components/LandingPage.jsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar'
 
 const LandingPage = () => {
   return (
-    <div className=' h-screen w-full'> 
+    <div className=' h-screen w-full bg-gray-100'> 
        <NavBar/>
     </div>
   )
