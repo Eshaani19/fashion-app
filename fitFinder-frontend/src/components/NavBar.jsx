a5be11c6bfe283454385cb81d06a8a5ee3c6f3ee
Eshaani19 <eshaaniarvind@gmail.com>
2024-07-01 13:18:29 +0530

    Update NavBar.jsx
    logo

--- fitFinder-frontend/src/components/NavBar.jsx
+++ fitFinder-frontend/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <nav className='bg-gradient-to-r from-pink-500 to-orange-400 backdrop-blur-sm h-24 fixed top-0 left-0 right-0 p-4 z-50'>
       <div className='container mx-auto flex justify-between items-center h-full  gap-[100px] font-bold text-lg font-sans  '>
         <nav className=''>
-          <a href="#" className='text-black '><img className="w-24 h-24 rounded-lg"src="public/logo3.png" alt='logo'></img></a>
+          <a href="#" className='text-black '><img className="w-24 h-24 rounded-lg"src="logo3.png" alt='logo'></img></a>
         </nav>
         <nav className='flex justify-center gap-36 items-center'>
         <a href="#" className='text-white hover:text-blue-200'>Home</a>
