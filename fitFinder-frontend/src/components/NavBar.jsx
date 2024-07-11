08e6194933d7f80d0340525face0c627a404722e
Eshaani Arvind <eshaaniarvind@gmail.com>
2024-07-11 17:12:32 +0530

    Update login,signup,FAQ,Account pages

--- fitFinder-frontend/src/components/NavBar.jsx
+++ fitFinder-frontend/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-
-const NavBar = () => {
+const NavBar = ({ loggedIn, handleLogout }) => {
   return (
     <nav className='bg-gradient-to-r from-pink-500 to-orange-400 backdrop-blur-sm h-24 fixed top-0 left-0 right-0 p-4 z-50'>
       <div className='container mx-auto flex justify-between items-center h-full  gap-[100px] font-bold text-lg font-sans  '>
@@ -9,13 +9,13 @@ const NavBar = () => {
           <a href="#" className='text-black '><img className="w-24 h-24 rounded-lg"src="logo3.png" alt='logo'></img></a>
         </nav>
         <nav className='flex justify-center gap-36 items-center'>
-        <a href="#" className='text-white hover:text-blue-200'>Home</a>
-        <a href="#" className='text-white hover:text-blue-200'>FAQ</a>
-        <a href="#" className='text-white hover:text-blue-200'>Login</a>
+          <Link to="/" className="text-white hover:text-blue-200">Home</Link>
+          <Link to="/faq" className="text-white hover:text-blue-200">FAQ</Link>
+          {!loggedIn && <Link to="/login" className="text-white hover:text-blue-200">Login</Link>}
+          {!loggedIn && <Link to="/signup" className="text-white hover:text-blue-200">Signup</Link>}
+          {loggedIn && <Link to="/account" className="text-white hover:text-blue-200">My Account</Link>}
+          {loggedIn && <button onClick={handleLogout} className="text-white">Logout</button>}
         </nav>
-        <div className='links flex justify-center items-center'>
-          {/* Add your links here */}
-        </div>
       </div>
     </nav>
   )
